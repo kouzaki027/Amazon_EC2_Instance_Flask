fee3bf1063a09d97d52fb5034c3217b0d477968f
kouzaki027 <112736093+kouzaki027@users.noreply.github.com>
2022-12-03 18:30:35 -0500

    automating script

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#1/bin/bash
+echo "starting script"
+
+#echo "Enter directory name"
+#read newdir
+#`mkdir $newdir`
+#mkdir COS1
+#cd COS1
+apt list --upgradable
+sudo apt update
+sudo apt-get update
+sudo apt install python3
+sudo apt-get install -y python3-pip
+sudo apt update
+sudo apt install pip3
+sudo pip3 install flask
+pip3 install -r requriements.txt
+sudo apt update
+#sudo pip3 install boto3
+#ls
